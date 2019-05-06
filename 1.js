cfe6f9308fc49f39a70d17f55cb39d027bccfa17
Thuan Nguyen <235thuan@gmail.com>
2019-05-06 14:37:07 -0700

    nop lan 1

---
+++ 1.js
@@ -0,0 +1,12 @@
+function bamyes() {
+    alert("<3 He he I don't love you")
+}
+let x= document.getElementById("no");
+
+function bamno() {
+
+    let a = Math.random()*window.innerHeight;
+   let b = Math.random()*window.innerWidth;
+   x.style.left = b +"px";
+   x.style.top = a +"px";
+}
